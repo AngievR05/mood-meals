45fa2eb48ad4c19f8938d29507e342435d54fae3
AngievR05 <159420956+AngievR05@users.noreply.github.com>
2025-07-28 19:03:07 +0200

    Add logo to Navbar and update favicon
    Introduced a new logo image and updated the Navbar to display it alongside the app name. Changed the favicon in index.html to use the new logo and updated related CSS for proper logo alignment and color consistency.

--- mood-meals/client/src/components/Navbar.jsx
+++ mood-meals/client/src/components/Navbar.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
+import logo from '../assets/images/Group2.png';
+
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-logo">
-        <Link to="/">🥣 Mood Meals</Link>
-      </div>
+    <div className="navbar-logo">
+      <Link to="/">
+        <img src={logo} alt="Mood Meals Logo" className="logo-img" />
+        Mood Meals
+      </Link>
+    </div>
       <ul className="navbar-links">
         <li><Link to="/tracker">Mood Tracker</Link></li>
         <li><Link to="/meals">My Meals</Link></li>
